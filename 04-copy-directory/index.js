ade534c419a3169e10d04489b2f70a23995281ac
Sashkakotov <sashkakotov@mail.ru>
2022-11-04 19:31:14 +0300

    feat: complete 04 task

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,27 @@
+const fs = require("fs");
+const fsPromises = fs.promises;
+const path = require("path");
 
+async function copyDirectory() {
+  await fsPromises.mkdir(path.join(__dirname, "files-copy"), {
+    recursive: true,
+  });
+  await fsPromises
+    .readdir(path.join(__dirname, "files"), { withFileTypes: true })
+    .then((files) => {
+      for (let file of files) {
+        fsPromises
+          .copyFile(
+            path.join(__dirname, "files", `${file.name}`),
+            path.join(__dirname, "files-copy", `${file.name}`)
+          )
+          .then(function () {
+            console.log(`File ${file.name} is copied`);
+          });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+copyDirectory();
